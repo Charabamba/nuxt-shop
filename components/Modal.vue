27defaf8cad03f7dc60ffe994e1321fffe111f85
Charabamba <voralse@mail.ru>
2021-02-05 01:51:24 +0500

    Create header and footer prototype

---
+++ components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="site-modal">
+    <div class="site-modal__wrapper">
+      <h2>Мы вам перезвоним!</h2>
+      <form>
+        <label>
+          Телефон
+          <input type="text" />
+        </label>
+        <label>
+          Ваше имя
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.site-modal {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(48, 48, 48, 0.301);
+  display: flex;
+  align-items: center;
+  .site-modal__wrapper {
+    padding: 40px 20px;
+    background-color: #ffffff;
+    margin: 0 auto;
+  }
+}
+</style>
--- components/system/Footer.vue
+++ components/system/Footer.vue
@@ -1,5 +1,30 @@
 <template>
   <footer>
-    <div class="container">footer</div>
+    <div class="container">
+      <div class="footer-info">
+        <p class="footer-info__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum
+          perspiciatis, dolorem quia error, necessitatibus voluptate minus odio
+          nulla neque aliquid sit perferendis unde ullam non excepturi est!
+          Recusandae, sed deleniti!
+        </p>
+        <p class="footer-info__copyright">Сделано мной</p>
+      </div>
+    </div>
   </footer>
 </template>
+
+<style lang="scss">
+.footer-info {
+  display: flex;
+  align-items: center;
+  padding: 10px 0px;
+  font-size: 12px;
+  .footer-info__text {
+    max-width: 800px;
+  }
+  .footer-info__copyright {
+    margin-left: auto;
+  }
+}
+</style>
