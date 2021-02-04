e2c9e5f59ea7148d085b0c63ca8254e854ec21c5
Charabamba <voralse@mail.ru>
2021-02-05 00:27:36 +0500

    Create base structure

---
+++ components/system/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <div class="container">
+      <nav class="site-nav">
+        <ul class="site-nav__list">
+          <li v-for="navLink in navLinks" :key="navLink.title">
+            <nuxt-link :to="navLink.url">
+              {{ navLink.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navLinks: [
+        {
+          title: "Главная",
+          url: "/",
+        },
+        {
+          title: "Каталог",
+          url: "/catalog/",
+        },
+        {
+          title: "Контакты",
+          url: "/contacts/",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/contacts/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container">Страница контактов</div>
+</template>
