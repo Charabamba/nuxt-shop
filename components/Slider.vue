e38ca0de1981afca086dbcf59fa937438156fad5
Charabamba <voralse@mail.ru>
2021-02-06 00:18:53 +0500

    Add slider

---
+++ components/Slider.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>
+        <img
+          src="https://images.pexels.com/photos/1080722/pexels-photo-1080722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.pexels.com/photos/1080722/pexels-photo-1080722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.pexels.com/photos/1080722/pexels-photo-1080722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+      </div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+export default {
+  name: "MyComponent",
+  components: { VueSlickCarousel },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
   <div class="container">
     <AppTitle title="Главная страница" />
     Главная страница
+
+    <slider />
   </div>
 </template>
 
+<script>
+import slider from "@/components/Slider.vue";
+export default {
+  components: {
+    slider,
+  },
+};
+</script>
