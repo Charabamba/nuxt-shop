245b815a76c2fb8e0e6f17c148fd3edabdec9b62
Charabamba <voralse@mail.ru>
2021-03-24 00:41:49 +0500

    add filter

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <AppTitle title="Каталог" />
-
+    <div class="container">
+      <appInput class="filter" label="Фильтр" v-model="filter" />
+    </div>
     <div class="container">
       <productsList :productsList="productsLoaded" />
     </div>
@@ -11,15 +13,36 @@
 
 <script>
 import productsList from "@/components/ProductsList.vue";
+import appInput from "@/components/UI/Input.vue";
 
 export default {
   components: {
     productsList,
+    appInput,
+  },
+  data() {
+    return {
+      filter: "",
+    };
   },
   computed: {
     productsLoaded() {
-      return this.$store.getters.getProductsLoaded;
+      let result =
+        this.filter.length > 2
+          ? this.$store.getters.getProductsLoaded.filter((product) => {
+              return product.title.includes(this.filter);
+            })
+          : this.$store.getters.getProductsLoaded;
+
+      return result;
     },
   },
 };
 </script>
+
+<style lang="scss">
+.filter {
+  margin-bottom: 20px;
+  max-width: 300px;
+}
+</style>
